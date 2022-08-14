a7833065317c67cfbc5df9799f1ab229e866d7ab
Coltmoons <coltmoonsac@gmail.com>
2022-08-13 22:40:46 -0500

    Create Operate function which direct the operation to it's own function

--- script.js
+++ script.js
@@ -17,3 +17,39 @@ function multiply(expression){
 function divide(expression){
     return expression[0] / expression[1];
 }
+//-------------------------------------------------------------//
+// split by operator works!
+function operate(expression){
+    const numbers = expression.split(' ').filter(op=>{
+        return !isNaN(op);
+    }).map(number =>{
+        return parseFloat(number);
+    });
+    numbers;
+    const operator = expression.split(' ').filter(op=>{
+        return isNaN(op);
+    }).join();
+
+    let result;
+    if(!(numbers.length === 2)) return 'Enter a valid operation'
+
+    switch (operator){
+        case '+':
+            result = add(numbers);
+            break;
+        case '-':
+            result = subtract(numbers);
+            break;
+        case '/':
+            result = divide(numbers);
+            break;
+        case '*':
+            result = multiply(numbers);
+            break;
+        default:
+            result = 'Please enter a valid operator';
+    }
+    return result;
+}
+
+console.log(operate('23 * '))
