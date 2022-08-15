7e59bcc8043e0e6a19f94c82d92cd5ad6fe9076e
Coltmoons <coltmoonsac@gmail.com>
2022-08-14 20:55:47 -0500

    Fix change of name variable in order to make code more readable

--- script.js
+++ script.js
@@ -1,55 +1,20 @@
-function add(expression){
-    return expression.reduce((total, data)=>{
-        return total += data;
+function add(numbers){
+    return numbers.reduce((total, number)=>{
+        return total += number;
     }, 0);
 }
 
-function subtract(expression){
-    return expression[0] - expression[1]
+function subtract(numbers){
+    return numbers[0] - numbers[1]
 }
 
-function multiply(expression){
-    return expression.reduce((total, data)=>{
-        return total *= data;
+function multiply(numbers){
+    return numbers.reduce((total, number)=>{
+        return total *= number;
     }, 1);
 }
 
-function divide(expression){
-    return expression[0] / expression[1];
+function divide(numbers){
+    return numbers[0] / numbers[1];
 }
 //-------------------------------------------------------------//
-// split by operator works!
-function operate(expression){
-    const numbers = expression.split(' ').filter(op=>{
-        return !isNaN(op);
-    }).map(number =>{
-        return parseFloat(number);
-    });
-    numbers;
-    const operator = expression.split(' ').filter(op=>{
-        return isNaN(op);
-    }).join();
-
-    let result;
-    if(!(numbers.length === 2)) return 'Enter a valid operation'
-
-    switch (operator){
-        case '+':
-            result = add(numbers);
-            break;
-        case '-':
-            result = subtract(numbers);
-            break;
-        case '/':
-            result = divide(numbers);
-            break;
-        case '*':
-            result = multiply(numbers);
-            break;
-        default:
-            result = 'Please enter a valid operator';
-    }
-    return result;
-}
-
-console.log(operate('23 * '))
