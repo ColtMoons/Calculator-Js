afb602cf9d6819ee5e254f5b7f40b0988778550d
Coltmoons <coltmoonsac@gmail.com>
2022-08-14 21:35:40 -0500

    Create function to operate 2 numbers

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//basic math
 function add(number1, number2){
     return number1 + number2;
 }
@@ -12,4 +13,29 @@ function multiply(number1, number2){
 
 function divide(number1, number2){
     return number1 / number2;
+}
+
+//Operate
+
+function operate(operator, number1, number2){
+    let result;
+
+    switch(operator){
+        case '+':
+            result = add(number1, number2);
+            break;
+        case '-':
+            result = subtract(number1, number2);
+            break;
+        case '*':
+            result = multiply(number1, number2);
+            break;
+        case '/':
+            result = divide(number1, number2);
+            break;
+        default:
+            result = 'No valid operator';
+    }
+
+    return result;
 }
